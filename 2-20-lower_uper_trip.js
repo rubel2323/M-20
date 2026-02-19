c3fac9324590b2ca19c8b8848615c751ef1daac1
Rubel <mr.adcadre41@gmail.com>
2026-02-19 08:21:01 +0600

    case and trim added

---
+++ 2-20-lower_uper_trip.js
@@ -0,0 +1,11 @@
+const boi = " Psychology";
+const toi = "psyChology ";
+if (boi === toi) {
+  console.log("asob boi toi amar valo lage na r");
+} else if (boi.toLowerCase().trim() === toi.toLowerCase().trim()) {
+  console.log("R valo ki lage ato trim ar case mela j jasce");
+} else if (boi.trim() === toi.trim()) {
+  console.log("asob boi ar toi melaiya trip kora hoyce");
+} else {
+  console.log("lekha pora na korle manush hobi kamne");
+}
