87b32e657c2bad905e08924d01de02cd7010cb22
Rubel <mr.adcadre41@gmail.com>
2026-02-19 12:27:28 +0600

    reverse added

---
+++ 00004.js
@@ -0,0 +1,14 @@
+const alpha = "you are not women";
+let rev = "";
+for (beta of alpha) {
+  rev = beta + rev;
+  //   console.log(rev);
+}
+// for (let i = 0; i < alpha.length; i++) {
+//   console.log(alpha[i] + "->" + i);
+//   console.log("\n");
+//   console.log(i);
+// // }
+// const gamma = alpha.split("").reverse();
+// console.log(gamma);
+console.log(rev);
