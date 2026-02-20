9b728f2a96e771040fe5b3941a3f52c2cdaabd73
Rubel <mr.adcadre41@gmail.com>
2026-02-20 09:27:03 +0600

    object added:

---
+++ object-2.js
@@ -0,0 +1,10 @@
+const carrierPlan = {
+  name: "assistant commissioner",
+  bcs_number: "52_bcs",
+  location: "dhaka",
+  merit: 2,
+  work: function () {
+    console.log("I am working for humanity");
+  },
+};
+carrierPlan.work();
---
+++ object.js
@@ -0,0 +1,11 @@
+const shikhaRubel_house = {
+  name: "Peace_house",
+  model: "5-storyed",
+  color: "white_colorful",
+  location: "kushtia",
+  attached_bathroom: true,
+  room: function () {
+    console.log("There is a doctor's room");
+  },
+};
+shikhaRubel_house.room();
