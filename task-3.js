233a230cee74cecb2b3b95e1a667296ee576d17a
Rubel <mr.adcadre41@gmail.com>
2026-02-20 19:31:13 +0600

    task -3 added

---
+++ task-3.js
@@ -0,0 +1,11 @@
+const student = {
+  name: "Hamim Sakep",
+  id: 5421,
+  physics: {
+    subject: "HSC Physics",
+    author: "Shahjahan Tapan",
+    marks: 30,
+  },
+};
+const obtainedMarks = student.physics.marks;
+console.log("The marks obtained is ", obtainedMarks);
